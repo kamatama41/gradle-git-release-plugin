4e722dd485b6cb1f258f64525bf9e5766ebee254
Shinichi Ishimura <shiketaudonko41@gmail.com>
2018-02-15 00:31:30 +0900

    Remove unnecessary !!

--- src/main/kotlin/com/github/kamatama41/gradle/gitrelease/extensions.kt
+++ src/main/kotlin/com/github/kamatama41/gradle/gitrelease/extensions.kt
@@ -3,4 +3,4 @@ package com.github.kamatama41.gradle.gitrelease
 import org.gradle.api.Project
 
 inline fun <reified T : Any> Project.the(): T =
-        convention.findPlugin(T::class.java) ?: convention.getByType(T::class.java)!!
+        convention.findPlugin(T::class.java) ?: convention.getByType(T::class.java)
